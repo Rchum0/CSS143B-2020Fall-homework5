2eb12cbfde519d3861311983c715345f3489e8d1
Chum <ravichum@gmail.com>
2020-11-13 20:03:53 -0800

    LevelOrder Finished Commit

--- src/main/java/Problem1/LevelOrderTraverse.java
+++ src/main/java/Problem1/LevelOrderTraverse.java
@@ -1,28 +1,45 @@
 package Problem1;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
-
+import java.util.Queue;
+/*
+https://www.geeksforgeeks.org/level-order-tree-traversal/
+ */
 public class LevelOrderTraverse {
     public static List<List<Integer>> levelOrder(TreeNode<Integer> root) {
         // homework
         List<List<Integer>> result = new ArrayList<>();
-        //Queue<TreeNode>
+        Queue<TreeNode<Integer>> data = new LinkedList<>();
 
+        if (root == null) {
+            return result;
+        }
 
+        List<Integer> list = new ArrayList<>();
+        data.add(root);
+        data.add(null);
 
-
-
-
-
-
-
-
-
-
-
-
-
+        while (data.size() > 0) {
+            TreeNode<Integer> temp = data.poll();
+            if (temp == null) {
+                result.add(list);
+                if (!data.isEmpty()) {
+                    data.add(null);
+                    list = new ArrayList<>();
+                }
+            } else {
+                if (temp.left != null) {
+                    data.add(temp.left);
+                }
+                if (temp.right != null) {
+                    data.add(temp.right);
+                }
+                list.add(temp.val);
+            }
+        }
         return result;  // place holder
     }
 }
+
--- src/main/java/Problem2/LCA.java
+++ src/main/java/Problem2/LCA.java
@@ -5,7 +5,10 @@ import Problem1.TreeNode;
 public class LCA {
     public static TreeNode<Integer> lowestCommonAncestor(TreeNode<Integer> root, TreeNode<Integer> p, TreeNode<Integer> q) {
         // homework
-        // hint: pay attention to the assumptions of this problem.
+        if(root == null){
+            return null;
+        }
+        if()
         return null;    // place holder
     }
 }
