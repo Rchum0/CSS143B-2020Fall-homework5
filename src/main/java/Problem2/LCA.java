039182b23808b3bd02babf31dbd55649ef2e92c3
Chum <ravichum@gmail.com>
2020-11-13 20:08:41 -0800

    LCA Finished Commit

--- src/main/java/Problem2/LCA.java
+++ src/main/java/Problem2/LCA.java
@@ -5,10 +5,26 @@ import Problem1.TreeNode;
 public class LCA {
     public static TreeNode<Integer> lowestCommonAncestor(TreeNode<Integer> root, TreeNode<Integer> p, TreeNode<Integer> q) {
         // homework
-        if(root == null){
+
+        if (root == null) {
             return null;
         }
-        if()
-        return null;    // place holder
+
+        if (root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        }
+
+        if (left != null) {
+            return left;
+        } else {
+            return right;
+        }
     }
 }
